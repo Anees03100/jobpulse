2d0864a75267f72753d01d55b5a4c2edc07ddd19
anees03100@gmail.com <anees03100@gmail.com>
2026-08-19 20:32:01 +0500

    buttons widget added

--- lib/delete.dart
+++ lib/delete.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:jobpulse/widgets/butons/primary_button.dart';
+import 'package:jobpulse/widgets/butons/secondary_button.dart';
 
 class Delete extends StatefulWidget {
   const Delete({super.key});
@@ -12,15 +14,31 @@ class _DeleteState extends State<Delete> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Delete')),
-      body: const Center(
-        child: Text(
-          'Daniyal',
-          style: TextStyle(
-            fontSize: 50,
-            fontWeight: FontWeight.bold,
-            color: Colors.red,
+      body: Column(
+        children: [
+          const Center(
+            child: Text(
+              'Daniyal',
+              style: TextStyle(
+                fontSize: 50,
+                fontWeight: FontWeight.bold,
+                color: Colors.red,
+              ),
+            ),
           ),
-        ),
+          PrimaryButton(
+            label: 'De',
+            onPressed: () {
+              // Handle delete action
+            },
+          ),
+          SecondaryButton(
+            label: 'Cancel',
+            onPressed: () {
+              // Handle cancel action
+            },
+          ),
+        ],
       ),
     );
   }
